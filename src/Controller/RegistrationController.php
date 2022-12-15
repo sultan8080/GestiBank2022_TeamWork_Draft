f7646be913a7ba62c24b7277204c68c9d1f9b808
Mariem <gharbi.mariem92@gmail.com>
2022-12-15 16:40:13 +0100

    maillig juste après enregistrement

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,27 +3,36 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\Mime\Email;
 use App\Form\RegistrationFormType;
 use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(MailerInterface $mailer,Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new Email())
+            ->from('gk@smart-it-partner.com')
+            ->to($user->getEmail())
+            ->subject('Validation Création de compte')
+            ->html('<H2>Félicitation votre demande a été validé et votre compte est desormais actif</H2>');
+
+            $mailer->send($email);
             // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
@@ -52,13 +61,21 @@ class RegistrationController extends AbstractController
         ]);
     }
     #[Route('/registerConseiller', name: 'app_registerConseiller')]
-    public function registerConseiller(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function registerConseiller(MailerInterface $mailer,Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
          $form->handleRequest($request);
+        
 
         if ($form->isSubmitted() && $form->isValid()) {
+             $email = (new Email())
+            ->from('gk@smart-it-partner.com')
+            ->to($form->get('email')->getdata())
+            ->subject('Validation Création de compte')
+            ->html('<H2>Félicitation votre demande a été validé et votre compte est desormais actif</H2><br><h3>votre login est :</h3><br><h3>et votre mot de passe est :');
+
+        $mailer->send($email);
              // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
@@ -80,4 +97,18 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
          ]);
     }
+    // #[Route('/email')]
+    // public function sendEmail(MailerInterface $mailer):Response
+    // {
+    //     $email = (new Email())
+    //         ->from('gk@smart-it-partner.com')
+    //         ->to('david.mona.mpro@gmail.com')
+    //         ->subject('Validation Création de compte')
+    //         ->html('<H2>Félicitation votre demande a été validé et votre compte est desormais actif</H2>');
+
+    //     $mailer->send($email);
+    //     return new Response("Email Send");
+
+    // }
+
 }
